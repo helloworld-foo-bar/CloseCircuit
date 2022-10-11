8288b72be23153a9ec2db4a40be098c908804112
Donnie <61128728+helloworld-foo-bar@users.noreply.github.com>
2022-10-11 18:42:34 -0500

    Save closecircuit.py.

--- closecircuit.py
+++ closecircuit.py
@@ -1,27 +1,26 @@
 class Ohms_law_cal:
-	
-	def __init__(self, V, I, R, call_id):
-		self.V = V
-		self.I = I
-		self.R = R
-		self.call_id = call_id
-		
-	def caller(self):
-		if self.call_id == "volts":
-			return self.volts(self.I, self.R)
-		elif self.call_id == "amps":
-			return self.amps(self.V, self.R)
-		elif self.call_id == "ohm":
-			return self.ohm(self.V, self.I)
-		else:
-			print("unknown input")
-		
-	def volts(self, I, R):
-		return I * R
-		
-	def amps(self, V, R):
-		return V / R
-		
-	def ohm(self, V, I):
-		return V / I
 
+def __init__(self, V, I, R, call_id):
+self.V = V
+self.I = I
+self.R = R
+self.call_id = call_id
+
+def caller(self):
+if self.call_id == "volts":
+return self.volts(self.I, self.R)
+elif self.call_id == "amps":
+return self.amps(self.V, self.R)
+elif self.call_id == "ohm":
+return self.ohm(self.V, self.I)
+else :
+print("unknown input")
+
+def volts(self, I, R):
+return I * R
+
+def amps(self, V, R):
+return V / R
+
+def ohm(self, V, I):
+return V / I
